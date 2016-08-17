ed17f936a0fde2d244ecdac02a750346d7876164
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-08-17 13:31:59 +1000

    MAINT: updated database tests for release 85

--- tests/test_database.py
+++ tests/test_database.py
@@ -14,7 +14,7 @@ __maintainer__ = "Gavin Huttley"
 __email__ = "Gavin.Huttley@anu.edu.au"
 __status__ = "alpha"
 
-release = 81
+release = 85
 
 if 'ENSEMBL_ACCOUNT' in os.environ:
     args = os.environ['ENSEMBL_ACCOUNT'].split()
@@ -62,9 +62,9 @@ class TestDatabase(TestCase):
 
     def test_get_table_row_counts(self):
         """should return correct row counts for some tables"""
-        expect = {'homo_sapiens_core_81_38.analysis': 61,
-                  'homo_sapiens_core_81_38.seq_region': 55616,
-                  'homo_sapiens_core_81_38.assembly': 102090}
+        expect = {'homo_sapiens_core_85_38.analysis': 61,
+                  'homo_sapiens_core_85_38.seq_region': 55616,
+                  'homo_sapiens_core_85_38.assembly': 102090}
         human = Database(account=account, release=release,
                          species='human', db_type='core')
         table_names = [n.split('.')[1] for n in expect]
