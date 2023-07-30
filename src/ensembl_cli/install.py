443d87dc70f3e5cf7152a1f7998baa1b0b6b3dcc
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-07-30 09:58:14 +0200

    ENH: implement installation of annotations

--- src/ensembl_cli/install.py
+++ src/ensembl_cli/install.py
@@ -3,7 +3,7 @@ import shutil
 
 import click
 
-from cogent3 import load_seq, open_
+from cogent3 import load_annotations, load_seq, open_
 from rich.progress import track
 from unsync import unsync
 
@@ -18,8 +18,20 @@ def _install_one_seq(src, dest_dir):
     return True
 
 
-def _install_gffdb(config, species):
-    ...
+@unsync(cpu_bound=True)
+def _install_one_annotations(src, dest):
+    if dest.exists():
+        return True
+
+    _ = load_annotations(path=src, write_path=dest)
+    return True
+
+
+def _install_gffdb(src_dir: os.PathLike, dest_dir: os.PathLike):
+    src_dir = src_dir / "gff3"
+    dest = dest_dir / "features.gff3db"
+    paths = list(src_dir.glob("*.gff3.gz"))
+    return [_install_one_annotations(path, dest) for path in paths]
 
 
 def _install_seqs(src_dir: os.PathLike, dest_dir: os.PathLike):
@@ -49,11 +61,12 @@ def local_install(configpath, force_overwrite):
         dest_dir = config.install_path / db_name
         tasks.extend(_install_seqs(src_dir, dest_dir))
 
+    # we now load the individual gff3 files and write to annotation db's
+    for db_name in config.db_names:
+        src_dir = config.staging_path / db_name
+        dest_dir = config.install_path / db_name
+        tasks.extend(_install_gffdb(src_dir, dest_dir))
     # we do all tasks in one go
-    _ = [
-        t.result() for t in track(tasks, description="Installing seqs", transient=True)
-    ]
-
-    # we copy the dowloaded sequence data to the new location
+    _ = [t.result() for t in track(tasks, description="Installing...", transient=True)]
 
     return config
