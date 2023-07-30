9ba18f2fe51d44de122e3b53413e4b5f5cd0285f
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-07-30 10:08:29 +0200

    DEV: updated noxfile

--- noxfile.py
+++ noxfile.py
@@ -7,17 +7,6 @@ _py_versions = range(8, 12)
 
 
 @nox.session(python=[f"3.{v}" for v in _py_versions])
-def test_slow(session):
-    session.install(".[test]")
-    session.chdir("tests")
-    session.run(
-        "pytest",
-        "-m",
-        "slow",
-    )
-
-
-@nox.session(python=[f"3.{v}" for v in _py_versions])
 def test(session):
     session.install(".[test]")
     session.chdir("tests")
@@ -28,9 +17,5 @@ def test(session):
         "--cov-report",
         f"lcov:lcov-{session.python}.info",
         "--cov",
-        "cogent3",
-        "--ignore",
-        "test_app_mpi.py",
-        "-m",
-        "not slow",
+        "ensembl_cli",
     )
