a4de92db3dbd4060dda7e7023d92817e2a58510e
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-08-05 16:57:11 +0100

    TST: move pytest fixture for modified config into conftest

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,31 +1,11 @@
 import os
 import shutil
 
-from configparser import ConfigParser
-
 import pytest
 
 from click.testing import CliRunner
 
 from ensembl_cli.cli import download, exportrc, install
-from ensembl_cli.util import get_resource_path
-
-
-@pytest.fixture(scope="function")
-def tmp_config(tmp_dir):
-    # create a simpler download config
-    # we want a very small test set
-    parser = ConfigParser()
-    parser.read(get_resource_path("ensembldb_download.cfg"))
-    parser.remove_section("C.elegans")
-    parser.remove_section("compara")
-    parser.set("local path", "staging_path", value=str(tmp_dir / "staging"))
-    parser.set("local path", "install_path", value=str(tmp_dir / "install"))
-    download_cfg = tmp_dir / "download.cfg"
-    with open(download_cfg, "wt") as out:
-        parser.write(out)
-
-    yield download_cfg
 
 
 def test_download(tmp_config):
